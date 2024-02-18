28167ab0c4acdf4f9b9e3fff24d54c082db2002c
Ben <benedicto.krisno7@gmail.com>
2024-02-18 15:00:03 -0500

    simple layout

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='Input'>
+        <div>
+          <p>Input</p>
+        </div>
+
+        <div>
+          <p>Factors</p>
+        </div>
+
+        <div>
+          <p>Aspect</p>
+        </div>
+
+        <div>
+          <p>Submit</p>
+        </div>
+      </div>
+
+      <div className='Output'>
+        <p>output</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
